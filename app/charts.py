1fa25e8b22bb788384fa2cd3620aa36c6c9db636
jhonyalexis100@gmail.com <jhonyalexis100@gmail.com>
2023-06-26 21:57:31 -0500

    using docker

--- app/charts.py
+++ app/charts.py
@@ -11,7 +11,7 @@ def generate_pie_chart(labels,values):
   fig, ax = plt.subplots()
   ax.pie(values, labels = labels)
   ax.axis('equal')
-  plt.savefig('./app/pie.png')
+  plt.savefig('./app/chart_pie.png')
   plt.close()
   
 
